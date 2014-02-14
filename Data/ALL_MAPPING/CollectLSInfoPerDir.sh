ba08c4aaffcc28a173addbc139038b2ad18de633
mturchin20 <mturchin20@gmail.com>
2014-02-14 10:37:00 -0600

    Removing files that had their names changed

--- Data/ALL_MAPPING/CollectLSInfoPerDir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for dir in `cat home_pg_ALL_Mapping_dirList.20130513.txt`
-do
-	ls -lrt /home/pg/ALL_MAPPING/$dir/. | awk '{ print $9 }' > $dir/home_pg_ALL_Mapping_${dir}_dirList.20130513.txt
-done
