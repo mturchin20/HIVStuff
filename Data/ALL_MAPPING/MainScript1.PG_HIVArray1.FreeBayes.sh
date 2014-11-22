c3a29dd51f8d84630f643189506b85b758cf41b4
mturchin20 <mturchin20@gmail.com>
2014-11-21 19:45:50 -0600

    20141121 -- Adding MainScript1.PG_HIVArray1.FreeBayes.sh to list

---
+++ Data/ALL_MAPPING/MainScript1.PG_HIVArray1.FreeBayes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+##20141121
+
+cp -p /data/userdata/pg/michaelt/Data/ALL_MAPPING/DataProcessing.Pipeline.pt6.Samtools.pta.CreateVCFfilesPerChromosome.Launcher.vs1.sh /data/userdata/pg/michaelt/Data/ALL_MAPPING/DataProcessing.Pipeline.pt6.FreeBayes.pta.CreateVCFfilesPerChromosome.Launcher.vs1.sh 
+
+cp -p /data/userdata/pg/michaelt/Data/ALL_MAPPING/DataProcessing.Pipeline.pt6.Samtools.pta.CreateVCFfilesPerChromosome.vs1.sh /data/userdata/pg/michaelt/Data/ALL_MAPPING/DataProcessing.Pipeline.pt6.FreeBayes.pta.CreateVCFfilesPerChromosome.vs1.sh
+
+mkdir /data/userdata/pg/michaelt/Data/ALL_MAPPING/Pools/MM5/PostMerge/mapping_pool_merged/FreeBayes
+
+
+
